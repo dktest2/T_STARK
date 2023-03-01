68d8bfd19b958934156d5559c3d853b4adcd3929
Hintpirox <120027363+Hintpirox@users.noreply.github.com>
2023-03-01 13:08:07 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,7 +1,7 @@
 if [ -z $UPSTREAM_REPO ]
 then
   echo "Cloning main Repository"
-  git clone https://github.com/Hontpirox/setapi.git /setapi
+  git clone https://github.com/Hintpirox/setapi.git /setapi
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
   git clone $UPSTREAM_REPO /setapi
